497a84e5af924e110cc553b763107a75335bf46b
Yevhenii Baibikov <jeka.spets333@gmail.com>
2026-04-27 12:48:27 +0200

    changed project structure

---
+++ server/orm/genre/create.js
@@ -0,0 +1,31 @@
+import DB from "../connect.js";
+
+const createGenre = (genreData, callback) => {
+    const { name, description, parent_id } = genreData;
+
+    if (!name || !description) {
+        return callback(new Error('Name and description are required fields.'));
+    }
+
+    const sql = `INSERT INTO genres (name, description, parent_id) VALUES (?, ?, ?)`;
+    DB.run(sql, [name, description, parent_id], function(err) {
+        if (err) {
+            console.error('Error creating genre:', err.message);
+            return callback(err);
+        }
+        console.log(`Genre created with ID: ${this.lastID}`);
+        callback(null, { id: this.lastID, name, description, parent_id });
+    });
+};
+
+createGenre({
+    name: 'Rock',
+    description: 'A genre of popular music that originated in the 1950s.',
+    parent_id: null
+}, (err, genre) => {
+    if (err) {
+        console.error('Error:', err.message);
+    } else {
+        console.log('Created Genre:', genre);
+    }
+});
