afbe1be6d4cd54a18251145215026a17bca56950
Yevhenii Baibikov <jeka.spets333@gmail.com>
2026-04-29 18:05:34 +0200

    added toggling favorite attribute for tracks

--- server/controller/track.js
+++ server/controller/track.js
@@ -1,10 +1,11 @@
 import router from "express";
 import createTrack from "../orm/tracks/create.js";
 import getTracks from "../orm/tracks/get.js";
+import toggleTrack from "../orm/tracks/toggle.js";
 
 const trackRouter = router.Router();
 
 trackRouter.post("/", createTrack);
 trackRouter.get("/", getTracks);
-
+trackRouter.post("/:id/favorite", toggleTrack);
 export default trackRouter;
