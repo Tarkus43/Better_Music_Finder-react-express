d6611dcd92a7c1e858fdc31f08daab90d8ed7b59
Yevhenii Baibikov <jeka.spets333@gmail.com>
2026-04-30 14:41:25 +0200

    added deleting genre route

--- server/controller/genre.js
+++ server/controller/genre.js
@@ -1,10 +1,12 @@
 import router from "express";
 import createGenre from "../orm/genre/create.js";
+import deleteGenre from "../orm/genre/delete.js";
 import listGenres from "../orm/genre/list.js";
 
 const genreRouter = router.Router();
 
 genreRouter.post("/", createGenre);
 genreRouter.get("/", listGenres);
+genreRouter.delete("/:id", deleteGenre);
 
 export default genreRouter;
